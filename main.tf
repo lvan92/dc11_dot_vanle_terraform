0ae114d0457e346a5ffb50d26e0215c049333abe
lvan92 <69503069+lvan92@users.noreply.github.com>
2023-09-29 16:03:53 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+terraform {
+  backend "s3" {
+    encrypt                 = true
+    key                     = "networking/terraform.tfstate"
+    bucket                  = "devops-vanle-terraform-state-s3"
+    profile                 = "default"
+    region                  = "us-east-1"
+    dynamodb_table = "terraform-state-dynamodb-table-lock"
+  }
+}
 
+provider "aws" {
+  region                   = "us-east-1"
+  profile                  = "default"
+}
